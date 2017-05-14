6097be59cfd1070b3f22ba735b54ba00ab671b8c
Evgeny Ukhanov <mrlsd@ya.ru>
2017-05-15 02:31:58 +0300

    Initial

--- encrypt.sh
+++ encrypt.sh
@@ -3,6 +3,7 @@ git clone github.com/mrlsd/gpg-test /tmp/auth-go/
 tar -cvpzf ../auth-kit /tmp/auth-go/auth-go.tar.gz
 gpg -e -r mrlsd@ya.ru /tmp/auth-go/auth-go.tar.gz
 rm -f /tmp/auth-go/auth-go.tar.gz
+св /tmp/auth-go/
 git add .
 git commit -a
 git push -u origin master
